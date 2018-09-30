c7846c5b847f7646ec03908b809431feee0dd2f0
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 14:43:06 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,7 @@ def can_be_found_updated_and_saved
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
   movie.save
-  
+  movie
 end
 
 def can_update_using_update_method
