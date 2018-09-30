2fdd6b034a245b8f76c5e729f9cd66f2cfbc1b74
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 13:52:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_09_30_134620) do
 
-  create_table "movies", force: :cascade do |t|
+  create_table "create_movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
