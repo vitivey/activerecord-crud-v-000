a477d782bac5e1fd4184b902340a3e0487e32ca4
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 14:20:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,10 +64,10 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.create(title: "Awesome Flick")
+  movie.title = "Even Awesomer Flick"
+  movie.update
+  movie.save
 end
 
 def can_update_using_update_method
