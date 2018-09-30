c9283b3d5da70401afb00096511251e65ff308af
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 14:49:59 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = title:
+    m.release_date = release_date:
   end
 end
 
