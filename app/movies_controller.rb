4311220b04d873f3b6b035d87000a2fd36c74d34
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 14:37:28 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,8 +72,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
+  movie = Movie.create(title: "Wat?")
+  movie.title = "Wat, huh?"
   __
 end
 
